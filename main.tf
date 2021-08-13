ef0c76ccd2569d49050bfc953eadd6afaaa0cb7f
Mayank Govilla <31316460+mgovilla@users.noreply.github.com>
2021-08-13 14:30:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0443305dabd4be2bc"
+  ami           = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
 
   tags = {
